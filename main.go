07186f5bb4cef5159a62a257700480c0ad3c385a
naveenkmavoor <naveenkmavoor1@gmail.com>
2021-09-12 13:54:14 +0530

    added faq page

--- main.go
+++ main.go
@@ -16,9 +16,18 @@ func contact(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "To get in touch please sent an email to <a href=\"shootmequest@gmail.com\">shootmequest.com</a>.")
 
 }
+func faq(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html")
+	fmt.Fprint(w, "<h1>Questions asked before</h1><br><h3>What does it mean to be an software engineer by passion</h3>")
 
+}
+ 
 func main() {
-	http.HandleFunc("/", home)
-	http.HandleFunc("/contact", contact)
-	http.ListenAndServe(":8082", nil)
+	mux := &http.ServeMux{}
+
+	mux.HandleFunc("/", home)
+	mux.HandleFunc("/faq", faq)
+	mux.HandleFunc("/contact", contact) 
+	http.NotFoundHandler()
+	http.ListenAndServe(":8082", mux)
 }
