9cb76f0b9a52e2e57e71b217b4ac7b1621d78251
naveenkmavoor <naveenkmavoor1@gmail.com>
2021-09-12 14:18:37 +0530

    Added a custom 404 page

--- main.go
+++ main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"fmt"
 	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
 func home(w http.ResponseWriter, r *http.Request) {
@@ -18,16 +20,19 @@ func contact(w http.ResponseWriter, r *http.Request) {
 }
 func faq(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html")
-	fmt.Fprint(w, "<h1>Questions asked before</h1><br><h3>What does it mean to be an software engineer by passion</h3>")
+	fmt.Fprint(w, "<h1>Questions asked before</h1><br><h3>What does it mean to be an software engineer by passion?</h3>")
 
 }
- 
+func notFound(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html")
+	w.WriteHeader(http.StatusNotFound)
+	fmt.Fprint(w, "<h1>Sorry, but we couldn't find the page you were looking for :(</h1>")
+}
 func main() {
-	mux := &http.ServeMux{}
-
-	mux.HandleFunc("/", home)
-	mux.HandleFunc("/faq", faq)
-	mux.HandleFunc("/contact", contact) 
-	http.NotFoundHandler()
-	http.ListenAndServe(":8082", mux)
+	router := mux.NewRouter()
+	router.HandleFunc("/", home)
+	router.HandleFunc("/faq", faq)
+	router.HandleFunc("/contact", contact)
+	router.NotFoundHandler = http.HandlerFunc(notFound)
+	http.ListenAndServe(":8082", router)
 }
