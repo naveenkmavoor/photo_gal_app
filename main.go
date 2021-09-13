b2de1bec3625db76d3a086641d4d0644810bd9e9
naveenkmavoor <naveenkmavoor1@gmail.com>
2021-09-13 16:32:52 +0530

    added templates

--- main.go
+++ main.go
@@ -1,34 +1,46 @@
 package main
 
 import (
-	"fmt"
 	"net/http"
+	"text/template"
 
 	"github.com/gorilla/mux"
 )
 
+var tpl *template.Template
+
 func home(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html")
-
-	fmt.Fprint(w, "<h1>Welcome to my awesome site!</h1>")
+	if err := tpl.ExecuteTemplate(w, "home.gohtml", nil); err != nil {
+		panic(err)
+	}
 }
 
 func contact(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html")
-	fmt.Fprint(w, "To get in touch please sent an email to <a href=\"shootmequest@gmail.com\">shootmequest.com</a>.")
-
+	if err := tpl.ExecuteTemplate(w, "contact.gohtml", nil); err != nil {
+		panic(err)
+	}
 }
 func faq(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html")
-	fmt.Fprint(w, "<h1>Questions asked before</h1><br><h3>What does it mean to be an software engineer by passion?</h3>")
-
+	if err := tpl.ExecuteTemplate(w, "faq.gohtml", nil); err != nil {
+		panic(err)
+	}
 }
 func notFound(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html")
 	w.WriteHeader(http.StatusNotFound)
-	fmt.Fprint(w, "<h1>Sorry, but we couldn't find the page you were looking for :(</h1>")
+	if err := tpl.ExecuteTemplate(w, "pagenotfound.gohtml", nil); err != nil {
+		panic(err)
+	}
+}
+func init() {
+	tpl = template.Must(template.ParseGlob("views/*.gohtml"))
+
 }
 func main() {
+
 	router := mux.NewRouter()
 	router.HandleFunc("/", home)
 	router.HandleFunc("/faq", faq)
